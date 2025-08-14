a4b0cbe02367c3d3c6d838fdb400ae471b8ae993
Ruslan <sounditbox@gmail.com>
2025-08-14 22:15:56 +0400

    added products app category and product models

--- config/settings.py
+++ config/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'users',
+    'products',
 
 ]
 
---
+++ products/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+class JournalizedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ('-created_at',)
+
+
+class Category(JournalizedModel):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True,
+                               blank=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/products/category/{self.slug}/'
+
+
+class Product(JournalizedModel):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    image = models.ImageField(upload_to='product_images', null=True, blank=True)
+    description = models.TextField(max_length=1000)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    stock = models.PositiveIntegerField()
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/products/{self.slug}/'
