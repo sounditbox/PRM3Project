03c3b7407f95c071eb2615f78a392269230a4929
Ruslan <sounditbox@gmail.com>
2025-08-18 21:57:43 +0400

    fixed gitignore, moved common template (base), implemented product list view

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ProductDetailView
+from .views import ProductDetailView, ProductListView
 
 app_name = 'products'
 
 
 urlpatterns = [
     path('products/<slug:slug>/', ProductDetailView.as_view(), name='product-detail'),
+    path('products/', ProductListView.as_view(), name='product-list'),
 ]
--- users/views.py
+++ users/views.py
@@ -9,9 +9,5 @@ class UserCreateView(CreateView):
     success_url = reverse_lazy('users:login')
 
 
-class HomeView(TemplateView):
-    template_name = 'users/home.html'
-
-
 class AccountView(TemplateView):
     template_name = 'users/account.html'
