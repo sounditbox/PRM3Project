38b85804c32d5f47b84018fac7b921d7537e16b3
Ruslan <sounditbox@gmail.com>
2025-08-21 18:44:02 +0400

    added guides-recipes.html view

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ProductDetailView, ProductListView
+from .views import ProductDetailView, ProductListView, GuidesRecipesView
 
 app_name = 'products'
 
@@ -8,4 +8,5 @@ app_name = 'products'
 urlpatterns = [
     path('products/<slug:slug>/', ProductDetailView.as_view(), name='product-detail'),
     path('products/', ProductListView.as_view(), name='product-list'),
+    path('guides-recipes/', GuidesRecipesView.as_view(), name='guides-recipes'),
 ]
